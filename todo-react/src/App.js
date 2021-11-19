504317c74b34486eddff809ecd459042d7df4839
Jodu555 <Jodu505@gmail.com>
2021-11-19 14:27:49 +0100

    tried to get a basic todo app working: failed

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,17 +1,31 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [todods, setTodos] = useState(new Map());
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-
+    setTodos([{ ID: 1212121, todo: 'Clean the table', checked: false }, { ID: 121223122, todo: 'Go out for a walk', checked: false }]);
+    console.log('Call');
   }, []);
 
 
   return (
     <div className="App">
 
-
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Todo</th>
+          <th>Checked</th>
+        </tr>
+        {todos.map((todo, idx) => {
+          return <tr>
+            <td>{todo.ID}</td>
+            <td>{todo.todo}</td>
+            <td>{todo.checked}</td>
+          </tr>;
+        })}
+      </table>
 
     </div>
   );
